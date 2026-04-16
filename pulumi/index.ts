7f707cc0953a46b808d67ec6ba19838284597bb3
iodine123 <iodinehanifan@gmail.com>
2026-04-16 23:34:54 +0700

    Fix:

--- pulumi/index.ts
+++ pulumi/index.ts
@@ -20,7 +20,7 @@ const cluster_config = {
     master: {
         count: 1,
         cpu: 2,
-        memory: 2048,
+        memory: 3072,
         diskSize: 20,
         ip_start: 5
     },
@@ -34,14 +34,30 @@ const cluster_config = {
     ansible: {
         cpu: 1,
         memory: 1024,   
-        diskSize: 5,
+        diskSize: 15,
         ip: 30
     },
     network: {
-        gateway: "192.168.10.1",
-        subnet: "192.168.10"
+        gateway: "192.168.198.2",
+        subnet: "192.168.198"
     }
 }
+
+const user = `#cloud-config
+users:
+  - name: ansible_user
+    groups: sudo
+    shell: /bin/bash
+    sudo: ['ALL=(ALL) NOPASSWD:ALL']
+    ssh_authorized_keys:
+      - ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAACAQCaqYWMxJQpyPJ7ud80wI+JI0r+KDMJg+vuGbH7dsu4IdN5bEr/gz/u7hBwiT2hCQq6Rk0jfiJOPPS9KRaQin7N+PD+y+51CwTZAt/sBhLdm2UYu/+PV/bcZOHn53SxVh0NcP1Vs1FuK+VgOOIWPl1kSVT88yy3/5ABAPRLDJBfDaUQoNXhJpJFJcltd0vuw+GAwkutB6dXo8m6SIvqWeHEqPbHdgzO7dSWcnpcSQGE4pqSTpypqS9q+SCPhDeVXojTUBHoHrbA4lPPbweEaJr+ja2+VSYcxP3A6huXJYUt9AmKwfBI6yI6cf7z3XJhq9crdDC2cvL9y4Y8y0xylrc9Kz8xZbLdMJ6wYveTG0D2ZqUewcPEvr9WRPAOLBsI7K3p6W1/s8caejWAZC034xru4/rh5SbLHlfs58bUGyYVqhOzeSXeFe8KVxXmx5t54iwYMBfmHMSXHxOy7o/LZbI64YmqnXwsUTBlKG6emeDe030I2lPYAWRSrzhYLRHo6fIgiW3jZx0yhVzF6oRNzWKVmPJvvxCCglnIMoKiXmvSGkx/kkSrgJ3bsfstE2EDEmid9eZTCNrZ+Efiabt8F+9lSXc6DPpISeTCxP07bOSGrQhz9a6aajlTjk7/qYeHA7MLbRNLgQ6fGjdTvaNyi/9XPupfruzVbxT7hslOF9Ciqw== iodinehanifan@gmail.com
+  - name: admin123
+    groups: sudo
+    shell: /bin/bash
+    sudo: ['ALL=(ALL) NOPASSWD:ALL']
+    ssh_authorized_keys:
+      - ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAACAQCaqYWMxJQpyPJ7ud80wI+JI0r+KDMJg+vuGbH7dsu4IdN5bEr/gz/u7hBwiT2hCQq6Rk0jfiJOPPS9KRaQin7N+PD+y+51CwTZAt/sBhLdm2UYu/+PV/bcZOHn53SxVh0NcP1Vs1FuK+VgOOIWPl1kSVT88yy3/5ABAPRLDJBfDaUQoNXhJpJFJcltd0vuw+GAwkutB6dXo8m6SIvqWeHEqPbHdgzO7dSWcnpcSQGE4pqSTpypqS9q+SCPhDeVXojTUBHoHrbA4lPPbweEaJr+ja2+VSYcxP3A6huXJYUt9AmKwfBI6yI6cf7z3XJhq9crdDC2cvL9y4Y8y0xylrc9Kz8xZbLdMJ6wYveTG0D2ZqUewcPEvr9WRPAOLBsI7K3p6W1/s8caejWAZC034xru4/rh5SbLHlfs58bUGyYVqhOzeSXeFe8KVxXmx5t54iwYMBfmHMSXHxOy7o/LZbI64YmqnXwsUTBlKG6emeDe030I2lPYAWRSrzhYLRHo6fIgiW3jZx0yhVzF6oRNzWKVmPJvvxCCglnIMoKiXmvSGkx/kkSrgJ3bsfstE2EDEmid9eZTCNrZ+Efiabt8F+9lSXc6DPpISeTCxP07bOSGrQhz9a6aajlTjk7/qYeHA7MLbRNLgQ6fGjdTvaNyi/9XPupfruzVbxT7hslOF9Ciqw== iodinehanifan@gmail.com
+`;
 
 // Create a new Proxmox VE virtual machine for the Kubernetes master node
 for (let i = 1; i <= cluster_config.master.count; i++) {
@@ -51,6 +67,9 @@ for (let i = 1; i <= cluster_config.master.count; i++) {
         clone: {
             vmId: 9001, 
             full: true,
+        },
+        agent: {
+            enabled: true,
         },
         cpu: {
             type: "host",
@@ -71,10 +90,7 @@ for (let i = 1; i <= cluster_config.master.count; i++) {
         // @ts-ignore
         initialization: {
             datastoreId: "local-lvm",
-            userAccount: {
-                username: "admin123",
-                password: "password123",
-            },
+            userData: user,
             ipConfigs: [{
                 ipv4: {
                     address: `${cluster_config.network.subnet}.${cluster_config.master.ip_start + i}/24`,       
@@ -96,6 +112,9 @@ for (let i = 1; i <= cluster_config.worker.count; i++) {
             vmId: 9002,
             full: true,
         },
+        agent: {
+            enabled: true,
+        },
         cpu: {
             type: "host",
             cores: cluster_config.master.cpu,
@@ -115,10 +134,7 @@ for (let i = 1; i <= cluster_config.worker.count; i++) {
         // @ts-ignore
         initialization: {
             datastoreId: "local-lvm",
-            userAccount: {
-                username: "admin123",
-                password: "password123",
-            },
+            userData: user,
             ipConfigs: [{
                 ipv4: {
                     address: `${cluster_config.network.subnet}.${cluster_config.worker.ip_start + i}/24`,
@@ -137,6 +153,9 @@ const ansible =new proxmox.vm.VirtualMachine(`ansible-server`, {
         clone: {
             vmId: 9002, 
             full: true,
+        },
+        agent: {
+            enabled: true,
         },
         cpu: {
             type: "host",
@@ -157,10 +176,7 @@ const ansible =new proxmox.vm.VirtualMachine(`ansible-server`, {
         // @ts-ignore
         initialization: {
             datastoreId: "local-lvm",
-            userAccount: {
-                username: "admin123",
-                password: "password123",
-            },
+            userData: user,
             ipConfigs: [{
                 ipv4: {
                     address: `${cluster_config.network.subnet}.${cluster_config.ansible.ip}/24`,       
