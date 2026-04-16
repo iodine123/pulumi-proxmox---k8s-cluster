ea1e2168678c273a8e3409bf52bb92129427bda6
iodine123 <iodinehanifan@gmail.com>
2026-04-17 00:08:12 +0700

    Fix: user config

--- pulumi/index.ts
+++ pulumi/index.ts
@@ -43,8 +43,6 @@ const cluster_config = {
     }
 }
 
-
-
 // Create a new Proxmox VE virtual machine for the Kubernetes master node
 for (let i = 1; i <= cluster_config.master.count; i++) {
     const vm =new proxmox.vm.VirtualMachine(`k8s-master-${i}`, {
@@ -76,7 +74,10 @@ for (let i = 1; i <= cluster_config.master.count; i++) {
         // @ts-ignore
         initialization: {
             datastoreId: "local-lvm",
-            userDataFileId: user,
+            userAccount: {
+                username: "admin123",
+                password: "password123",
+            },
             ipConfigs: [{
                 ipv4: {
                     address: `${cluster_config.network.subnet}.${cluster_config.master.ip_start + i}/24`,       
@@ -120,7 +121,10 @@ for (let i = 1; i <= cluster_config.worker.count; i++) {
         // @ts-ignore
         initialization: {
             datastoreId: "local-lvm",
-            userDataFileId: user,
+            userAccount: {
+                username: "admin123",
+                password: "password123",
+            },
             ipConfigs: [{
                 ipv4: {
                     address: `${cluster_config.network.subnet}.${cluster_config.worker.ip_start + i}/24`,
@@ -163,8 +167,8 @@ const ansible =new proxmox.vm.VirtualMachine(`ansible-server`, {
         initialization: {
             datastoreId: "local-lvm",
             userAccount: {
-                username: "ansible",
-                password: "ansible",
+                username: "admin123",
+                password: "password123",
             },
             ipConfigs: [{
                 ipv4: {
