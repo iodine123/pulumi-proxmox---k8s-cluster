270bf68e8798fffb58e92788986e7628cb77c687
iodine123 <iodinehanifan@gmail.com>
2026-04-17 07:13:52 +0700

    Ehancement!: add server configuration on server template

--- packer/scripts/common.sh
+++ packer/scripts/common.sh
@@ -1,13 +1,44 @@
 #!/bin/bash
 set -e
 
+#install basic packages
 sudo apt-get update && sudo apt-get install -y apt-transport-https ca-certificates curl sudo gnupg2 software-properties-common
 
-sudo apt-get install -y docker.io
-sudo systemctl enable --now docker
+#install containerd
+sudo apt-get install -y containerd
 
+#install kubeadm, kubelet, kubectl
 sudo curl -fsSL https://pkgs.k8s.io/core:/stable:/v1.29/deb/Release.key | sudo gpg --dearmor -o /etc/apt/keyrings/kubernetes-apt-keyring.gpg
 echo 'deb [signed-by=/etc/apt/keyrings/kubernetes-apt-keyring.gpg] https://pkgs.k8s.io/core:/stable:/v1.29/deb/ /' | sudo tee /etc/apt/sources.list.d/kubernetes.list
 sudo apt-get update
 sudo apt-get install -y kubelet kubeadm kubectl
 sudo apt-mark hold kubelet kubeadm kubectl
+
+# Disable swap
+sudo swapoff -a
+sudo sed -i '/ swap / s/^\(.*\)$/#\1/g' /etc/fstab
+
+# Setup modules & sysctl
+cat <<EOF | sudo tee /etc/modules-load.d/k8s.conf
+overlay
+br_netfilter
+EOF
+
+# Load modules
+sudo modprobe overlay
+sudo modprobe br_netfilter
+
+# Setup required sysctl params
+cat <<EOF | sudo tee /etc/sysctl.d/k8s.conf
+net.bridge.bridge-nf-call-iptables  = 1
+net.bridge.bridge-nf-call-ip6tables = 1
+net.ipv4.ip_forward                 = 1
+EOF
+
+sudo sysctl --system
+
+#setup containerd
+sudo mkdir -p /etc/containerd
+containerd config default | sudo tee /etc/containerd/config.toml
+sudo sed -i 's/SystemdCgroup = false/SystemdCgroup = true/g' /etc/containerd/config.toml
+sudo systemctl restart containerd
